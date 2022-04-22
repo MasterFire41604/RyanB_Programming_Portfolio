5ea945dd2cf020fa07e1ec8600040407637ae8b9
Ryan Beard <ryan.beard@tooeleschools.org>
2022-04-22 14:26:32 -0600

    Ryan_Beard, Added to project 2, 4/22/22

--- Project 2/TileBased/settings.py
+++ Project 2/TileBased/settings.py
@@ -21,14 +21,25 @@ PLAYER_IMG = "manBlue_gun.png"
 PLAYER_HIT_RECT = pg.Rect(0, 0, 35, 35)
 BARREL_OFFSET = vec(30, 10)
 
-# Gun settings
+# Weapon settings
 BULLET_IMG = "bullet.png"
-BULLET_SPEED = 500
-BULLET_LIFETIME = 1000
-BULLET_RATE = 150
-KICKBACK = 200
-GUN_SPREAD = 10
-BULLET_DAMAGE = 10
+WEAPONS = {}
+WEAPONS["pistol"] = {"bullet_speed": 500,
+                        "bullet_lifetime": 1000,
+                        "rate": 250,
+                        "kickback": 200,
+                        "spread": 5,
+                        "damage": 10,
+                        "bullet_size": "lg",
+                        "bullet_count": 1}
+WEAPONS["shotgun"] = {"bullet_speed": 400,
+                         "bullet_lifetime": 500,
+                         "rate": 900,
+                         "kickback": 300,
+                         "spread": 20,
+                         "damage": 5,
+                         "bullet_size": "sm",
+                         "bullet_count": 12}
 
 # Mob settings
 MOB_IMG = "zombie1_hold.png"
@@ -38,10 +49,12 @@ MOB_HIT_RECT = pg.Rect(0, 0, 30, 30)
 MOB_DAMAGE = 10
 MOB_KNOCKBACK = 20
 AVOID_RADIUS = 50
+DETECT_RADIUS = 400
 
 # Effects
 MUZZLE_FLASHES = ["whitePuff15.png", "whitePuff16.png", "whitePuff17.png", "whitePuff18.png"]
 FLASH_DURATION = 40
+SPLAT = "splat green.png"
 
 # Layers
 WALL_LAYER = 1
@@ -49,6 +62,26 @@ PLAYER_LAYER = 2
 BULLET_LAYER = 3
 MOB_LAYER = 2
 EFFECTS_LAYER = 4
+ITEMS_LAYER = 1
+
+# Items
+ITEM_IMAGES = {"Health": "health_pack.png",
+               "shotgun": "obj_shotgun.png"}
+HEALTH_PACK_AMOUNT = 20
+BOB_RANGE = 15
+BOB_SPEED = 0.3
+
+# Sounds
+BG_MUSIC = "espionage.ogg"
+PLAYER_HIT_SOUNDS = ["pain_8.wav", "pain_9.wav", "pain_10.wav", "pain_11.wav"]
+ZOMBIE_MOAN_SOUNDS = ["brains2.wav", "brains3.wav", "zombie-roar-1.wav", "zombie-roar-2.wav", "zombie-roar-3.wav",
+                      "zombie-roar-5.wav", "zombie-roar-6.wav", "zombie-roar-7.wav"]
+ZOMBIE_HIT_SOUNDS = ["splat-15.wav"]
+WEAPON_SOUNDS = {"pistol": ["pistol.wav"],
+                 "shotgun": ["shotgun.wav"]}
+EFFECTS_SOUNDS = {"level_start": "level_start.wav",
+                  "health_up": "health_pack.wav",
+                  "gun_pickup": "gun_pickup.wav"}
 
 # define colors
 WHITE = (255, 255, 255)
